e9368aead3717fbd97d1ad38211319dc860a46bf
Nimrod Shefi <nimrodshefi01@gmail.com>
2025-05-10 17:37:51 +0100

    creating a reusable button component with dependencies allowing for different css based on parameters with utils cleaning the css so it is consitent and not repetative

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/ui/Button";
+import React, { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <div>
+      <Button>hello</Button>
+    </div>
+  );
+};
+
+export default page;
