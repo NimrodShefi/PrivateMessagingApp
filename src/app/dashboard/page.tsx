651df37e018fdc40cd7f3847d7f93bea545209ef
Nimrod Shefi <nimrodshefi01@gmail.com>
2025-05-10 19:54:14 +0100

    Adding login session information to view on page. Also adding a necessary env value in: NEXTAUTH_SECRET

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,14 @@
 import Button from "@/components/ui/Button";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 import React, { FC } from "react";
 
 interface pageProps {}
 
-const page: FC<pageProps> = ({}) => {
-  return (
-    <div>
-      <Button>hello</Button>
-    </div>
-  );
+const page = async ({}) => {
+  const session = await getServerSession(authOptions);
+
+  return <pre>{JSON.stringify(session)}</pre>;
 };
 
 export default page;
