126c701d5a16078ce7cd02ce336c89f3c17c4c5c
Nimrod Shefi <nimrodshefi01@gmail.com>
2025-05-10 18:36:49 +0100

    creating the login screen with the logic and login screen - but login button isn't yet functional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import { db } from "@/lib/db";
+import Button from "@/components/ui/Button";
 
 export default async function Home() {
-  await db.set("hello", "hello");
-
-  return <div className="text-red-500">hello world</div>;
+  return (
+    <>
+      <Button variant="ghost" size="lg">
+        Hello
+      </Button>
+    </>
+  );
 }
